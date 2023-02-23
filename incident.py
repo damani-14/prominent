1e0e902b31d3d014d76a214fe26043f3230a5c30
Damani Driver <damanidriver@gmail.com>
2023-02-23 16:57:02 -0600

    Initial commit. Added inicident class, project data, and main.

---
+++ incident.py
@@ -0,0 +1,41 @@
+
+
+class Incident:
+
+    def __init__(self, **kwargs):
+
+        self.address = kwargs.get('address', {})
+        self.apparatus = kwargs.get('apparatus', [])
+        self.description = kwargs.get('description', {})
+        self.fire_department = kwargs.get('fire_department', {})
+        self.version = kwargs.get('version', {})
+
+    def get_address(self):
+        return self.address
+
+    def set_address(self, address):
+        self.address = address
+
+    def get_apparatus(self):
+        return self.apparatus
+
+    def set_apparatus(self, apparatus):
+        self.apparatus = apparatus
+
+    def get_description(self):
+        return self.description
+
+    def set_description(self, description):
+        self.description = description
+
+    def get_fire_department(self):
+        return self.fire_department
+
+    def set_fire_department(self, fire_department):
+        self.fire_department = fire_department
+
+    def get_version(self):
+        return self.version
+
+    def set_version(self, version):
+        self.version = version
---
+++ main.py
@@ -0,0 +1,11 @@
+import pandas as pd
+
+from incident import Incident
+
+
+def main():
+    pass
+
+
+if __name__ == "__main__":
+    main()
