8ad21c304cff7a1da024465d8dca639e9b1b857f
Damani Driver <damanidriver@gmail.com>
2023-02-23 19:35:18 -0600

    main.py: added data input.

--- main.py
+++ main.py
@@ -1,10 +1,25 @@
+import json
+import os
 import pandas as pd
 
 from incident import Incident
+from unit import Unit
 
 
 def main():
-    pass
+    json_path = 'data'
+    json_list = []
+    incident_list = []
+
+    for file in os.listdir(json_path):
+        if file.endswith('.json'):
+            json_list.append(file)
+
+    for i, j in enumerate(json_list):
+        with open(os.path.join(json_path, j)) as json_file:
+            incident_json = json.load(json_file)
+
+        incident_list.append(incident_json)
 
 
 if __name__ == "__main__":
