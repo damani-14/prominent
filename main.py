fd300ec6519ed01ff1667490574fb958509c2934
Damani Driver <damanidriver@gmail.com>
2023-02-24 11:23:01 -0600

    main.py: removed unit builder call. added console output for enriched incident data.

--- main.py
+++ main.py
@@ -1,10 +1,13 @@
+import geopandas as gpd
 import incident_builder
 import incident_enricher
 import json
 import os
+import sys
 import pandas as pd
 import unit_builder
 
+from geojson import MultiPoint, Point
 from incident import Incident
 from unit import Unit
 
@@ -25,8 +28,11 @@ def main():
         incidents.append(incident_json)
 
     incidents = incident_builder.create_incidents(incidents)
-    incidents = unit_builder.create_units(incidents)
+    # incidents = unit_builder.create_units(incidents)
     incidents = incident_enricher.enrich(incidents)
+
+    for incident in incidents:
+        sys.stdout.write(str(incident.__dict__))
 
 
 if __name__ == "__main__":
